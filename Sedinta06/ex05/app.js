97054353aeb278254115de85667849ad2dabf28f
Olivia <test@yyycom>
2019-04-18 22:05:48 +0300

    sedinta06 ex 5,6

--- Sedinta06/ex05/app.js
+++ Sedinta06/ex05/app.js
@@ -1,5 +1,30 @@
 (function() {
   document.addEventListener('DOMContentLoaded', function() {
-    
+    var showButton = document.querySelector('#showButton'),
+      toggleEventButton = document.querySelector('#toggleEventButton'),
+      paragraph = document.createElement('p');
+      eventBound = true;
+
+    function showAlert() {
+      alert('Butonul a fost apasat');
+    }
+
+    showButton.addEventListener('click', showAlert);
+    paragraph.innerText = 'Alerta va fi afisata';
+    document.body.appendChild(paragraph);
+
+    toggleEventButton.addEventListener('click', function() {
+      if (eventBound) {
+        showButton.removeEventListener('click', showAlert);
+        this.innerText = 'POrneste afisarea';
+        paragraph.innerText = 'Alerta nu va fi afisata';
+        eventBound = false;
+      } else {
+        showButton.addEventListener('click', showAlert);
+        this.innertext = 'Opreste afisarea';
+        paragraph.innerText = 'Alerta va fi afisata';
+        eventBound = true;
+      }
+    });
   });
 })();
