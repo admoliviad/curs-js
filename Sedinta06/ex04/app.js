28c61296b7aa5910912c24d06074a362d099c677
Olivia <test@yyycom>
2019-04-18 21:43:55 +0300

    sedinta06 ex1,2,3,4

--- Sedinta06/ex04/app.js
+++ Sedinta06/ex04/app.js
@@ -1,5 +1,26 @@
-(function() {
-  document.addEventListener('DOMContentLoaded', function() {
-      
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
+
+    var stage = document.querySelector('.stage');
+
+    stage.addEventListener('mouseover', function () {
+     log('Mouseul este pe scena');
+
+    });
+
+    stage.addEventListener('mouseover', function () {
+      log('Mouseul nu este pe scena');
+    });
+
+    function log(msg) {
+      var paragraphElement = document.createElement('p');
+
+      paragraphElement.innerText = msg;
+
+      paragraphElement.classList.add('message')
+
+      document.body.appendChild(paragraphElement);
+    }
+
   });
 })();
